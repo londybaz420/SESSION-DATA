099999b966c1f7a62d4c24d77aec54da7c81f16c
Bandaheali CDN <cdn@bandaheali.com>
2025-08-26 11:46:24 +0500

    Upload file: capnel.js

---
+++ uploads/1756190784726_capnel.js
@@ -0,0 +1,13 @@
+Control panel username:	b4_39528820
+Control panel password:	**********
+Control panel URL:	cpanel.byethost4.com
+MySQL username	b4_39528820
+MySQL password:	**********
+MySQL hostname:	sql301.byethost4.com
+FTP username:	b4_39528820
+FTP password:	**********
+FTP host name:	ftp.byethost4.com
+Your Website URL:	http://bandaheali.byethost4.com
+ 	 
+
+Thank you for choosing u
